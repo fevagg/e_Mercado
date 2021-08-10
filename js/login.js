767ed2f78203bda66349dab813b5e132529dabb6
Fernando Vargas <paingrille19@gmail.com>
2021-08-10 00:22:47 -0300

    Login y productos actualizados

--- js/login.js
+++ js/login.js
@@ -2,5 +2,24 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    var login = () =>{
+        var user = {};
+        var loginBtn = document.getElementById('ingresar');
+        var userLogin = document.getElementById('username');
+        var userPass = document.getElementById('password');
+        loginBtn.addEventListener('click', ()=>{
+            if(userLogin.value.trim() === ''){
+                alert('Necesita un nombre');
+            }else if(userPass.value.trim()=== ''){
+                alert('Necesita una contraseña');
+            }else{ 
+                location.href = 'principal.html';
+                user.nombre = userLogin.value;
+                user.contraseña = userPass.value;
+                user.estado = "conectado";
+                sessionStorage.setItem('usuario', JSON.stringify(user));
+            }
+        });
+    }
+    login();
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,38 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+import {getJSONData, PRODUCTS_URL} from './init.js'
 
+let products = [];
+
+let showProducts = (array)=>{
+    let productList = "";
+    for(let product of array){
+        productList += `<div class="list-group-item">
+        <div class="row">
+            <div class="col-sm-4">
+        <img src="${product.imgSrc}" alt="${product.description}" class="img-thumbnail">
+    </div>
+    <div class="col-sm-8">
+        <div class="d-flex w-100 justify-content-between">
+            <h4>${product.name}</h4>
+            <p>${product.soldCount} artículos</p>
+        </div>
+        <p>${product.description}</p>
+        <span>${product.currency} ${product.cost}</span>
+    </div>
+</div>
+</div>`
+    }
+    document.getElementById('product-list').innerHTML += productList;
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(response=>{
+        if(response.status === 'ok'){
+          products = response.data;
+          console.log(products)
+          showProducts(products);
+        }
+      })
 });
