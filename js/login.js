d11ec60b3fcb999a88cdf382f6c0b6fcbc482bff
fevagg <paingrille19@gmail.com>
2021-08-16 11:30:42 -0300

    OAuth

--- js/login.js
+++ js/login.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         loginBtn.addEventListener('click', ()=>{
             if(userLogin.value.trim() === ''){
                 alert('Necesita un nombre');
-            }else if(userPass.value.trim()=== ''){
+            }else if(userPass.value.trim() === ''){
                 alert('Necesita una contraseña');
             }else{ 
                 location.href = 'principal.html';
@@ -19,6 +19,19 @@ document.addEventListener("DOMContentLoaded", function(e){
                 user.estado = "conectado";
                 sessionStorage.setItem('usuario', JSON.stringify(user));
             }
+            function onSignIn(googleUser) {
+                // Useful data for your client-side scripts:
+                var profile = googleUser.getBasicProfile();
+                console.log('Full Name: ' + profile.getName());
+                console.log('Given Name: ' + profile.getGivenName());
+                console.log('Family Name: ' + profile.getFamilyName());
+                console.log("Image URL: " + profile.getImageUrl());
+                console.log("Email: " + profile.getEmail());
+        
+                // The ID token you need to pass to your backend:
+                var id_token = googleUser.getAuthResponse().id_token;
+                console.log("ID Token: " + id_token);
+              }
         });
     }
     login();
